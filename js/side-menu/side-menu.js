132d43d96cb9dad6694b8dd6fb733e8687083931
James <james.urron+1@gmail.com>
2026-03-08 22:30:06 +0000

    kana update

--- js/side-menu/side-menu.js
+++ js/side-menu/side-menu.js
@@ -66,7 +66,7 @@ var list = [
     ["","toggleColors(event)", "-function dark-mode"],
     ["","", 0],
     ["日本語の定番チートシート","window.scrollTo(0,0)", 1],
-    ["仮名（ＷＲＩＴＩＮＧ　ＳＹＳＴＥＭ）","scrollById(event,'kana-title')", 1],
+    ["仮名（ＫＡＮＡ）","scrollById(event,'kana-title')", 1],
     ["平仮名（ＨＩＲＡＧＡＮＡ）","scrollById(event,'hiragana-title')", 2],
     ["片仮名（ＫＡＴＡＫＡＮＡ）","scrollById(event,'katakana-title')", 2],
     ["漢字（ＫＡＮＪＩ）","scrollById(event,'kanji-title')", 1],
